83fc1cb5075c89d798e7d4b960c7d5d030b0ff58
auztinc1 <aaustinc@live.com>
2023-06-09 00:23:30 -0400

    It Just Works

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,115 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import './index.css';
 import socketClient  from "socket.io-client";
 const socket = socketClient.connect("http://localhost:3001/");
 
 function App() {
   
-  const [P1, setP1] = useState("")
-  const [P2, setP2] = useState("")
+  const [P1, setP1] = useState({id: "", hand: "🖐"})
+  const [P2, setP2] = useState({id: "", hand: "🖐"})
+  const [localHand, setLocalHand] = useState("🖐")
+  const [enemyHand, setEnemyHand] = useState("🖐")
   
-  const [enemy, setEnemy] = useState("");
   
-  socket.on("assign_player", (data) => {
-    
-    let pn = data.indexOf(socket.id)
-    
-    console.log("You are user #" + pn)
-    console.log("Your user id is " + socket.id)
-  })
+  // Setting client as Player 1 has succeeded
+  // Set local P1 state
   
   socket.on("p1_success", p1 => {
-    console.log(p1 + " set as Player 1")
-    setP1(p1)
+    let id = p1.id
+    let hand = P1.hand
+    setP1({id, hand})
   })
+  
+  // Setting client as Player 1 has failed
+  // Do nothing
   
   socket.on("p1_fail", p1 => {
-    console.log(p1 + " failed to set as Player 2")
+    console.log(p1.id + " failed to set as Player 2")
   })
   
+  // Setting client as Player 2 has succeeded
+  // Set local P2 state
+  
   socket.on("p2_success", p2 => {
-    setP2(p2)
+    let id = p2.id
+    let hand = P2.hand
+    setP2({id, hand})
   })
   
   socket.on("p2_fail", p2 => {
-    console.log(p2 + " failed to set as Player 2")
+    console.log(p2.id + " failed to set as Player 2")
   })
   
+  // When server broadcasts other player's hand
+  
+  socket.on("recieve_hand", ([p1, p2]) => {
+    // console.log(p1)
+    setP1(p1)
+    setP2(p2)
+    
+    if (socket.id == p1.id) {
+      setEnemyHand(p2.hand)
+    } else if (socket.id == p2.id) {
+      setEnemyHand(p1.hand)
+    } else {
+      setLocalHand(p1.hand)
+      setEnemyHand(p2.hand)
+    }
+    
+  })
+  
+  // When user clicks a button to choose hand
+  
   function handleClick(hand) {
-    socket.emit("send_message", hand)
+    
+    if (socket.id == P1.id) {
+      let id = P1.id;
+      setP1({id, hand})
+    }
+    if (socket.id == P2.id) {
+      let id = P2.id;
+      setP2({id, hand})
+    }
+    
+    // socket.emit("send_hand", hand)
   }
+  
+  useEffect(() => {
+    if (socket.id == P1.id) {
+      setLocalHand(P1.hand)
+    }
+    if (socket.id == P2.id) {
+      setLocalHand(P2.hand)
+    }
+  }, [P1, P2])
+
+  
+  function handleSubmit(){
+    socket.emit("submit_hand", localHand)
+  }
+  
     
   return (
     <div className="App">
       <div className="p-wrap">
         <button onClick={() => {socket.emit("setP1", socket.id)}} className="playerBtn">PLAYER WON</button>
-        <div className="p-id">{P1}</div>
+        <div className="p-id">{P1.id}</div>
       </div>
       <div className="p-wrap">
         <button onClick={() => {socket.emit("setP2", socket.id)}} className="playerBtn">PLAYER TOO</button>
-        <div className="p-id">{P2}</div>
+        <div className="p-id">{P2.id}</div>
       </div>
       {/* {console.log(socket.id)} */}
       
-      <div id="enemy">🖐</div>
+      <div id="enemy-hand">{enemyHand}</div>
+      <div id="local-hand">{localHand}</div>
       
       <div className="hand-wrap">
         <button onClick={() => {handleClick("✊")}} className="gameBtn rock">✊</button>
         <button onClick={() => {handleClick("✋")}} className="gameBtn paper">✋</button>
         <button onClick={() => {handleClick("✌")}} className="gameBtn scissors">✌</button>
       </div>
+      <button onClick={() => {handleSubmit()}}>Submit</button>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ app.use(cors());
 
 let users = []
 
-let p1 = ""
-let p2 = ""
+let p1 = {id: "", hand: ""}
+let p2 = {id: "", hand: ""}
 
 
 const io = new Server(http, {
@@ -20,16 +20,46 @@ const io = new Server(http, {
 })
 
 io.on('connection', (socket) => {
+	
 	users.push(socket.id)
 	console.log(socket.id + " has connected")
-	socket.emit("assign_player", users)
-	socket.on("send_message", (data) => {
-		console.log(socket.id + " plays " + data)
+	
+	socket.on("send_hand", (hand) => {
+		// if (socket.id === p1.id){
+		// 	p1.hand = hand;
+		// } 
+		// if (socket.id === p2.id){
+		// 	p2.hand = hand;
+		// }
+		
+		// console.log(socket.id + " plays " + data)
+		console.log(p2.hand)
+	})
+	
+	socket.on("submit_hand", (hand) => {
+		
+		
+		if (socket.id === p1.id){
+			p1.hand = hand;
+		} 
+		if (socket.id === p2.id){
+			p2.hand = hand;
+		}
+		
+		setTimeout(function() {
+			if (p1.hand !== "" && p2.hand !== "") {
+				socket.broadcast.emit("recieve_hand", [p1, p2])
+				socket.emit("recieve_hand", [p1, p2])
+				p1.hand = ""
+				p2.hand = ""
+			}
+		}, 1000)
+		
 	})
 	
 	socket.on("setP1", P1 => {
-		if (p1 === "") {
-			p1 = P1;
+		if (p1.id === "") {
+			p1.id = P1;
 			console.log("Player 1 is " + P1)
 			socket.broadcast.emit("p1_success", p1)
 			socket.emit("p1_success", p1)
@@ -38,8 +68,8 @@ io.on('connection', (socket) => {
 		}
 	})
 	socket.on("setP2", P2 => {
-		if (p2 === "") {
-			p2 = P2;
+		if (p2.id === "") {
+			p2.id = P2;
 			console.log("Player 2 is " + P2)
 			socket.broadcast.emit("p2_success", p2)
 			socket.emit("p2_success", p2)
@@ -51,12 +81,15 @@ io.on('connection', (socket) => {
 	socket.on("disconnect", () => {
 		users.splice(users.indexOf(socket.id)-1, 1)
 		
-		if (socket.id === p1) {
-			p1 = ""
+		if (socket.id === p1.id) {
+			p1.id = ""
 		}
-		if (socket.id === p2) {
-			p2 = ""
+		if (socket.id === p2.id) {
+			p2.id = ""
 		}
+		
+		
+		
 		console.log(socket.id + " has disconnected")
 	})
 })
